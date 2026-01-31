e9a804662ef5d3290bd38d8a7fa281c495e6ec8e
Bhuvnesh singh <cebhuvneshsingh@gmail.com>
2026-02-01 03:13:01 +0530

    commit

--- server/routes/proxy.js
+++ server/routes/proxy.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/proxy-image', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const { url } = req.query;
     if (!url) return res.status(400).send('URL required');
