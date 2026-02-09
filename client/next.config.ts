ad8bf84f3a6a38d9fc261f81d367bd5bdd9a3fad
Bhuvnesh singh <cebhuvneshsingh@gmail.com>
2026-02-09 23:17:58 +0530

    commit

--- client/next.config.ts
+++ client/next.config.ts
@@ -20,6 +20,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: '**' }, // Video thumbnails ,
       { protocol: 'http', hostname: '**' }  // Video thumbnails
     ],
+    path:'/public/',
     unoptimized: true, // Disable image optimization for external images
   }
 };
