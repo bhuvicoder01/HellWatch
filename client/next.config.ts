ccbac92ba06389255830242776a728f4f9cbf151
Bhuvnesh singh <cebhuvneshsingh@gmail.com>
2026-02-09 15:16:48 +0530

    commit

--- client/next.config.ts
+++ client/next.config.ts
@@ -14,6 +14,7 @@ const nextConfig: NextConfig = {
       }
     ];
   },
+  trailingSlash: true,
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' }, // Video thumbnails ,
